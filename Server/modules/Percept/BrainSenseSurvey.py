a7ad291846ca55bbacfa6af43d6c8e6b8c649966
Jackson Cagle <jackson.cagle@gmail.com>
2023-05-13 19:18:30 -0400

    Update BrainSense Survey to handle new data structure

--- Server/modules/Percept/BrainSenseSurvey.py
+++ Server/modules/Percept/BrainSenseSurvey.py
@@ -128,22 +128,24 @@ def querySurveyResults(user, patientUniqueID, authority):
             survey = Database.loadSourceDataPointer(recording.recording_datapointer)
             if not "Spectrum" in survey.keys():
                 survey = processBrainSenseSurvey(survey)
-                Database.saveSourceFiles(survey, "BrainSenseSurvey", survey["Channel"], recording.recording_id, recording.device_deidentified_id)
-            data = dict()
-            if device.device_name == "":
-                data["DeviceName"] = str(device.deidentified_id) if not (user.is_admin or user.is_clinician) else device.getDeviceSerialNumber(key)
-            else:
-                data["DeviceName"] = device.device_name
-            data["Timestamp"] = recording.recording_date.timestamp()
-            data["Channel"], data["Hemisphere"] = Percept.reformatChannelName(recording.recording_info["Channel"])
-            for lead in leads:
-                if lead["TargetLocation"].startswith(data["Hemisphere"]):
-                    data["Hemisphere"] = lead["TargetLocation"]
-                    break
-            data["Frequency"] = survey["Spectrum"]["Frequency"]
-            data["MeanPower"] = np.mean(survey["Spectrum"]["Power"],axis=1).tolist()
-            data["StdPower"] = SPU.stderr(survey["Spectrum"]["Power"],axis=1).tolist()
-            BrainSenseData.append(data)
+                Database.saveSourceFiles(survey, "BrainSenseSurvey", "Combined", recording.recording_id, recording.device_deidentified_id)
+
+            for i in range(len(survey["ChannelNames"])):
+                data = dict()
+                if device.device_name == "":
+                    data["DeviceName"] = str(device.deidentified_id) if not (user.is_admin or user.is_clinician) else device.getDeviceSerialNumber(key)
+                else:
+                    data["DeviceName"] = device.device_name
+                data["Timestamp"] = recording.recording_date.timestamp()
+                data["Channel"], data["Hemisphere"] = Percept.reformatChannelName(survey["ChannelNames"][i])
+                for lead in leads:
+                    if lead["TargetLocation"].startswith(data["Hemisphere"]):
+                        data["Hemisphere"] = lead["TargetLocation"]
+                        break
+                data["Frequency"] = survey["Spectrum"][i]["Frequency"]
+                data["MeanPower"] = np.mean(survey["Spectrum"][i]["Power"],axis=1).tolist()
+                data["StdPower"] = SPU.stderr(survey["Spectrum"][i]["Power"],axis=1).tolist()
+                BrainSenseData.append(data)
     return BrainSenseData
 
 def processBrainSenseSurvey(survey, method="spectrogram"):
@@ -161,6 +163,8 @@ def processBrainSenseSurvey(survey, method="spectrogram"):
     """
 
     [b,a] = signal.butter(5, np.array([1,100])*2/250, 'bp', output='ba')
-    filtered = signal.filtfilt(b, a, survey["Data"])
-    survey["Spectrum"] = SPU.defaultSpectrogram(filtered, window=1.0, overlap=0.5,frequency_resolution=0.5, fs=250)
+    survey["Spectrum"] = []
+    for i in range(len(survey["ChannelNames"])):
+        filtered = signal.filtfilt(b, a, survey["Data"][:,i])
+        survey["Spectrum"].append(SPU.defaultSpectrogram(filtered, window=1.0, overlap=0.5, frequency_resolution=0.5, fs=survey["SamplingRate"]))
     return survey
