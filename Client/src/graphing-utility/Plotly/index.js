452b02d368c3cface1a736822c35a1a20f3b7d0f
Jackson Cagle <jackson.cagle@gmail.com>
2023-09-07 17:00:20 -0400

    Permit downloading SVG for easier editing with Illustrator

--- Client/src/graphing-utility/Plotly/index.js
+++ Client/src/graphing-utility/Plotly/index.js
@@ -930,23 +930,44 @@ class PlotlyRenderManager {
    * Call to update figure or initial render
    */
   render() {
+    const config = {
+      responsive: true, locale: this.locale,
+      toImageButtonOptions: {
+        format: 'png', // one of png, svg, jpeg, webp
+        scale: 1
+      },
+      modeBarButtonsToAdd: [
+        {
+          name: 'Download Vector File',
+          icon: {
+            width: 857.1,
+            height: 1000,
+            path: 'm214-7h429v214h-429v-214z m500 0h72v500q0 8-6 21t-11 20l-157 156q-5 6-19 12t-22 5v-232q0-22-15-38t-38-16h-322q-22 0-37 16t-16 38v232h-72v-714h72v232q0 22 16 38t37 16h465q22 0 38-16t15-38v-232z m-214 518v178q0 8-5 13t-13 5h-107q-7 0-13-5t-5-13v-178q0-8 5-13t13-5h107q7 0 13 5t5 13z m357-18v-518q0-22-15-38t-38-16h-750q-23 0-38 16t-16 38v750q0 22 16 38t38 16h517q23 0 50-12t42-26l156-157q16-15 27-42t11-49z',
+            transform: 'matrix(1 0 0 -1 0 850)'
+          },
+          click: function(gd) {
+            const elementStyle = window.getComputedStyle(gd);
+            Plotly.toImage(gd, {
+              format:'svg',
+              width: elementStyle.width.replace("px",""),
+              height: elementStyle.height.replace("px","")
+            }).then((url) => {
+              var downloader = document.createElement('a');
+              downloader.href = url;
+              downloader.target = '_blank';
+              downloader.download = gd.id + '.svg';
+              downloader.click();
+            });
+          }
+        },
+      ],
+    }
+
     if (this.fresh) {
-      Plotly.newPlot(this.divName, this.traces, this.layout, {
-        responsive: true, locale: this.locale,
-        toImageButtonOptions: {
-          format: 'png', // one of png, svg, jpeg, webp
-          scale: 1
-        }
-      });
+      Plotly.newPlot(this.divName, this.traces, this.layout, config);
       this.fresh = false;
     } else {
-      Plotly.react(this.divName, this.traces, this.layout, {
-        responsive: true, locale: this.locale,
-        toImageButtonOptions: {
-          format: 'png', // one of png, svg, jpeg, webp
-          scale: 1
-        }
-      });
+      Plotly.react(this.divName, this.traces, this.layout, config);
     }
   }
 
