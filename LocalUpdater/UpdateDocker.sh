558acc94b72d9d83ea6a204168140f9681c9e34c
Jackson Cagle <jackson.cagle@gmail.com>
2024-07-02 10:55:01 -0400

    Move Electron Application Outside the main repository

--- LocalUpdater/UpdateDocker.sh
+++
@@ -1,4 +0,0 @@
-curl -O https://uf-bravo.jcagle.solutions/static/docker-compose.yml
-docker compose pull && docker compose -p bravo up -d
-docker container prune -f && docker image prune -f
-rm docker-compose.yml 
--- LocalUpdater/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  updateDocker: () => ipcRenderer.send('RequestDockerUpdate'),
-  onDockerUpdated: (callback) => ipcRenderer.on('UpdateDocker', (_event, value) => callback(value))
-});
