d34eef1a7bfc103ac058a34b3a3ca82e786e62c0
Jackson Cagle <jackson.cagle@gmail.com>
2024-09-19 15:12:56 -0400

    New Template for Beta/Gamma Analysis Pipeline

---
+++ Client/src/views/Experimental/CustomizedAnalysis/ProcessingTemplates/ExtractBetaGammaAnalysisTemplate.js
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import MDBox from "components/MDBox";
+import MDButton from "components/MDButton";
+import MDTypography from "components/MDTypography";
+import { Autocomplete, TextField, Switch, FormControlLabel } from "@mui/material";
+
+import { createFilterOptions } from "@mui/material/Autocomplete";
+const filter = createFilterOptions();
+
+const ExtractBetaGammaAnalysisTemplate = ({availableRecordings, setConfiguration}) => {
+  const [options, setOptions] = useState({
+    targetRecording: "",
+    cardiacRemoved: false,
+    filtered: false,
+    normalized: false,
+    averageDuration: 10,
+    psdMethod: "Welch's Periodogram",
+    output: "Beta/Gamma Features Output"
+  });
+
+  const checkInputComplete = () => {
+    return options.targetRecording !== "" && options.output !== "";
+  }
+
+  return (
+    <MDBox style={{marginTop: 20, paddingTop: 5, paddingBottom: 15}}>
+      <Autocomplete 
+        selectOnFocus 
+        clearOnBlur
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            placeholder={"Select Target Recording Type"}
+          />
+        )}
+        sx={{marginBottom: 2}}
+        filterOptions={(options, params) => {
+          const filtered = filter(options, params);
+          const { inputValue } = params;
+          return filtered;
+        }}
+        isOptionEqualToValue={(option, value) => {
+          return option === value;
+        }}
+        renderOption={(props, option) => <li {...props}>{option}</li>}
+        value={options.targetRecording}
+        options={availableRecordings}
+        onChange={(event, newValue) => setOptions({...options, targetRecording: newValue})}
+      />
+      
+      <FormControlLabel 
+        control={<Switch checked={options.cardiacRemoved} 
+        onChange={() => setOptions({...options, cardiacRemoved: !options.cardiacRemoved})} />} 
+        label="Apply Cardiac Filter? " />
+
+      <FormControlLabel 
+        control={<Switch checked={options.filtered} 
+        onChange={() => setOptions({...options, filtered: !options.filtered})} />} 
+        label="Filtered between 1-100Hz? " />
+
+      <FormControlLabel 
+        control={<Switch checked={options.normalized} 
+        onChange={() => setOptions({...options, normalized: !options.normalized})} />} 
+        label="Normalize PSDs?" />
+
+      <Autocomplete 
+        selectOnFocus 
+        clearOnBlur
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            label={"Select PSD Method: "}
+            placeholder={"Select PSD Method"}
+          />
+        )}
+        filterOptions={(options, params) => {
+          const filtered = filter(options, params);
+          const { inputValue } = params;
+          return filtered;
+        }}
+        isOptionEqualToValue={(option, value) => {
+          return option === value;
+        }}
+        renderOption={(props, option) => <li {...props}>{option}</li>}
+        value={options.psdMethod}
+        options={["Short-time Fourier Transform", "Welch's Periodogram", "Autoregressive (AR) Model"]}
+        onChange={(event, newValue) => setOptions({...options, psdMethod: newValue})}
+        style={{paddingTop: 30}}
+        />
+
+      <TextField
+        variant="standard"
+        margin="dense"
+        value={options.averageDuration}
+        placeholder={"Disable"}
+        onChange={(event) => setOptions({...options, averageDuration: event.target.value})}
+        label={"Spectral Window Size (seconds)"} type={"number"}
+        autoComplete={"off"}
+        fullWidth
+      />
+
+      <TextField
+        variant="standard"
+        margin="dense"
+        value={options.output}
+        placeholder={"Disable"}
+        onChange={(event) => setOptions({...options, output: event.target.value})}
+        label={"Output Result Label"} type={"text"}
+        autoComplete={"off"}
+        fullWidth
+      />
+      <MDBox style={{display: "flex", paddingLeft: 15, paddingRight: 15, paddingTop: 15, justifyContent: "flex-end"}}>
+        <MDButton color={"secondary"} 
+          onClick={() => setConfiguration(false)}
+        >
+          {"Cancel"}
+        </MDButton>
+        <MDButton color={"info"} 
+          onClick={() => {
+            if (checkInputComplete()) {
+              setConfiguration(options);
+            }
+          }} style={{marginLeft: 10}}
+        >
+          {"Update"}
+        </MDButton>
+      </MDBox>
+    </MDBox>
+  );
+};
+
+export default ExtractBetaGammaAnalysisTemplate;
--- Client/src/views/Experimental/CustomizedAnalysis/ProcessingTemplates/index.js
+++ Client/src/views/Experimental/CustomizedAnalysis/ProcessingTemplates/index.js
@@ -1,5 +1,6 @@
 import ExtractPSDsFromAnnotationsTemplate from "./ExtractPSDsFromAnnotationsTemplate";
 import ExtractNarrowBandSpectralTemplate from "./ExtractNarrowBandSpectralTemplate";
+import ExtractBetaGammaAnalysisTemplate from "./ExtractBetaGammaAnalysisTemplate";
 import { availableProcessings } from "../AnalysisSteps";
 
 const availableTemplates = [{
@@ -8,6 +9,9 @@ const availableTemplates = [{
 }, {
   value: "extractNarrowBandSpectral",
   label: "Extract Narrow-band Gamma from Recording",
+}, {
+  value: "extractBetaGammaAnalysis",
+  label: "Extract Beta/Gamma Power Trend During Streaming",
 }];
 
 const ProcessingTemplates = ({type, ...rest}) => {
@@ -86,6 +90,45 @@ const ProcessingTemplates = ({type, ...rest}) => {
       stepConfig = availableProcessings.filter((a) => a.type.value == "extractNarrowBandFeature")[0];
       steps.push({ ...stepConfig, targetRecording: startSignal, output: startSignal + "_GammaFeatures", frequencyRangeStart: 50, frequencyRangeEnd: 100, threshold: options.threshold });
       startSignal += "_GammaFeatures"; 
+
+      rest.setConfiguration(steps)
+    }}/>
+  } else if (type === "extractBetaGammaAnalysis") {
+    return <ExtractBetaGammaAnalysisTemplate availableRecordings={rest.availableRecordings} setConfiguration={(options) => {
+      if (!options) (rest.setConfiguration(false));
+
+      let steps = [];
+      let stepConfig = {};
+      let startSignal = options.targetRecording;
+      if (options.cardiacRemoved) {
+        stepConfig = availableProcessings.filter((a) => a.type.value == "cardiacFilter")[0];
+        steps.push({ ...stepConfig, targetRecording: startSignal, output: startSignal + "_CardiacFiltered" });
+        startSignal += "_CardiacFiltered"; 
+      }
+      if (options.filtered) {
+        stepConfig = availableProcessings.filter((a) => a.type.value == "filter")[0];
+        steps.push({ ...stepConfig, targetRecording: startSignal, highpass: "1", lowpass: "100", output: startSignal + "_Filtered" });
+        startSignal += "_Filtered"; 
+      }
+      
+      stepConfig = availableProcessings.filter((a) => a.type.value == "extractTimeFrequencyAnalysis")[0];
+      steps.push({ ...stepConfig, targetRecording: startSignal, output: startSignal + "_Spectrogram", psdMethod: options.psdMethod, window: options.averageDuration*1000, overlap: options.averageDuration*500 });
+      startSignal += "_Spectrogram"; 
+      
+      if (options.normalized) {
+        stepConfig = availableProcessings.filter((a) => a.type.value == "normalize")[0];
+        steps.push({ ...stepConfig, targetRecording: startSignal, highEdge: "90", lowEdge: "70", normalizeMethod: "FOOOF", output: startSignal + "_Normalized" });
+        startSignal += "_Normalized"; 
+      }
+
+      stepConfig = availableProcessings.filter((a) => a.type.value == "calculateSpectralFeatures")[0];
+      steps.push({ ...stepConfig, targetRecording: startSignal, output: startSignal + "_SpectralFeatures", bands: [
+        ["LowBeta", 12, 20], 
+        ["HighBeta", 20,30], 
+        ["Beta", 12,30], 
+        ["LowGamma", 30, 60],
+        ["HighGamma", 60, 90]
+      ] });
 
       rest.setConfiguration(steps)
     }}/>
